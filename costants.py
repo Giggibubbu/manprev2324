dec131dedd1dddf89e187be1bf3b94d212c8fc1b
martinamammarella <marty.mammarella@gmail.com>
2024-08-28 22:13:20 +0200

    modifichefepermodelli

--- costants.py
+++ costants.py
@@ -6,7 +6,7 @@ arp_fields = ['arp.hw.type', 'arp.proto.type', 'arp.hw.size', 'arp.proto.size']
 icmp_fields = ['icmp.type', 'icmp.code', 'icmp.checksum', 'icmp.checksum.status']
 ip_fields = ['ip.version', 'ip.hdr_len', 'ip.dsfield','ip.len', 'ip.id', 'ip.flags', 'ip.ttl', 'ip.proto', 'ip.checksum', 'ip.checksum.status', 'ip.src', 'ip.dst']
 tcp_fields = ['tcp.srcport', 'tcp.dstport', 'tcp.stream', 'tcp.len', 'tcp.seq', 'tcp.nxtseq', 'tcp.ack',
-            'tcp.hdr_len', 'tcp.flags', 'tcp.window_size_value', 'tcp.window_size', 'tcp.window_size_scalefactor', 'tcp.checksum',
+            'tcp.hdr_len', 'tcp.flags', 'tcp.window_size_value', 'tcp.window_size', 'tcp.checksum',
             'tcp.checksum.status', 'tcp.urgent_pointer']
 udp_fields = ['udp.srcport', 'udp.dstport', 'udp.port', 'udp.length', 'udp.checksum', 'udp.checksum_status']
 mbtcp_fields = ['mbtcp.trans_id', 'mbtcp.prot_id', 'mbtcp.len', 'mbtcp.unit_id']
